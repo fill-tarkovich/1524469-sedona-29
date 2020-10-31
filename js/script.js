5bf090e73660fb05122b288e524b8710871be7a5
fill-tarkovich <fill-tarkovich@users.noreply.github.com>
2020-10-31 15:52:17 +0300

    Финал

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let btn = document.querySelector(".form-header");
 let form = document.querySelector(".hotel-search");
-btn.onclick = function () {
+let area = form.querySelector(".form-date");
+btn.addEventListener("click", function (evt) {
   form.classList.toggle("hidden");
-};
+});
+form.addEventListener("submit", function(){
+  console.log("sending")
+});
