e65b47078b4e6766a3e9e8a2073ea9c4fcde17ea
fill-tarkovich <fill-tarkovich@users.noreply.github.com>
2020-11-02 15:51:18 +0300

    Правки

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@ let form = document.querySelector(".hotel-search");
 let date = form.querySelectorAll(".form-date");
 let number = form.querySelectorAll(".form-number");
 let search = form.querySelector(".search-button");
+let map = document.querySelector(".map");
 
 form.classList.add("hidden");
+map.classList.remove("hidden");
+form.classList.add("bounce");
 
 btn.addEventListener("click", function () {
   form.classList.toggle("hidden");
 })
 
-search.addEventListener("click", function (evt) {
+search.onclick = function() {
   if (!date.value || !number.value) {
-    form.classList.add("required")
-  } else {
-    form.classList.remove("required")
+    form.classList.add("required");}
   }
-})
